ab20ed5a89c179a54909daadb26437c7bed13150
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-15 16:44:42 -0400

    Added FlatList implementation (that is not working as intended) for reference to review later.

--- components/DestinationViewer/DestinationViewer.tsx
+++ components/DestinationViewer/DestinationViewer.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text, ScrollView, Animated, PanResponder } from 'react-native';
+import { StyleSheet, View, Text, FlatList, Animated} from 'react-native';
 import Drag from './Drag';
 
 const destinations = ['San Diego', 'Los Angeles', 'San Francisco', 'Portland', 'Seattle']
@@ -37,3 +37,112 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
 });
+
+
+/* FlatList implementation
+Currently this FlatList implementation will disable drag movement
+Also nativeEvent.layout will receive x and y coordinates relative to itself
+which is both 0 for each item and not relative to the parent component.
+*/
+
+// const sampleTrip: Object = {
+//   id: 100,
+//   destinations: [
+//     {
+//       city: 'San Francisco',
+//       POIs: [
+//         {
+//           id: 1,
+//           name: 'Golden Gate Bridge',
+//           details: 'An iconic red bridge'
+//         },
+//         {
+//           id: 2,
+//           name: 'Dolores Park',
+//           details: 'A city park'
+//         },
+//         {
+//           id: 3,
+//           name: 'Dumpling Home',
+//           details: 'A Chinese restaurant specializing in dumplings'
+//         }
+//       ]
+//     },
+//     {
+//       city: 'San Diego',
+//       POIs: [
+//         {
+//           id: 4,
+//           name: 'Legoland',
+//           details: 'An amusement park featuring Legos'
+//         },
+//         {
+//           id: 5,
+//           name: 'Tacos El Gordo',
+//           details: 'A famous Mexican restaurant'
+//         },
+//         {
+//           id: 6,
+//           name: 'UC San Diego',
+//           details: 'A public university'
+//         }
+//       ]
+//     },
+//     {
+//       city: 'Los Angeles',
+//       POIs: [
+//         {
+//           id: 7,
+//           name: 'Griffith Observatory',
+//           details: 'An observatory with telecopes and exhibits'
+//         },
+//         {
+//           id: 8,
+//           name: 'Disneyland',
+//           details: 'The happiest place on Earth'
+//         },
+//         {
+//           id: 9,
+//           name: 'Hollywood',
+//           details: 'A hub for entertainment and media'
+//         }
+//       ]
+//     }
+//   ]
+
+// };
+
+// export default function DestinationViewer() {
+
+//   const renderDestination = ({ item }) => (
+//     <Drag destination={item.city} />
+//   );
+
+//   return (
+//     <View style={styles.wrapper}>
+//       <View style={styles.header}>
+//         <Text>Hello</Text>
+//       </View>
+//       <FlatList contentContainerStyle={styles.body} data={sampleTrip.destinations} renderItem={renderDestination} />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   wrapper: {
+//     flex: 1,
+//   },
+//   header: {
+//     flex: 0.2,
+//     backgroundColor: '#219EBC',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   body: {
+//     flex: 0.8,
+//     backgroundColor: '#FB8500',
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//   },
+// });
