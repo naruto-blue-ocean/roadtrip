6e78079b615b1d5d78189692f9cd5a83dc62837f
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-15 00:47:40 -0400

    Single component could move vertically without glitching now

--- components/DestinationViewer/DestinationViewer.tsx
+++ components/DestinationViewer/DestinationViewer.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Animated, PanResponder } from 'react-native';
 
 const destinations = ['San Diego', 'Los Angeles', 'San Francisco', 'Portland', 'Seattle']
 
 export default function DestinationViewer() {
+  const offset = useRef(new Animated.Value(0)).current;
+
+  const panResponder = useRef(PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      offset.setValue(0);
+     },
+    onPanResponderMove: Animated.event([null, { dy: offset }], {useNativeDriver: false}),
+    onPanResponderRelease: () => { offset.stopAnimation((lastOffset) => { offset.setOffset(lastOffset); }); },
+  })).current;
+
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.header}>
         <Text>Hello</Text>
       </View>
-      <ScrollView style={styles.body}>
+      <View style={styles.body}>
+        <Animated.View style={{...styles.item, transform: [{ translateY: offset }]}} {...panResponder.panHandlers}>
+          <View />
+        </Animated.View>
+        <View style={styles.item} />
+        <View style={styles.item} />
+      </View>
+      {/* <ScrollView style={styles.body}>
         {destinations.map((destination) => (<Text key={destination}>{destination}</Text>))}
-      </ScrollView>
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+      </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -24,12 +46,22 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
   header: {
-    flex: 0.2,
+    flex: 1,
     backgroundColor: '#219EBC',
     justifyContent: 'center',
     alignItems: 'center',
   },
   body: {
+    flex: 4,
     backgroundColor: '#FB8500',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  item : {
+    backgroundColor: 'white',
+    height: '20%',
+    width: '30%',
+    borderColor: 'black',
+    borderWidth: 1,
   }
 });
