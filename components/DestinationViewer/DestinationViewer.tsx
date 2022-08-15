462878f3682f5668697bd5e56e08438c51adaa29
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-15 01:19:38 -0400

    Animated elements can now be modularized and reused

--- components/DestinationViewer/DestinationViewer.tsx
+++ components/DestinationViewer/DestinationViewer.tsx
@@ -1,21 +1,11 @@
 import React, { useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, ScrollView, Animated, PanResponder } from 'react-native';
+import MainDrag from './MainDrag';
 
 const destinations = ['San Diego', 'Los Angeles', 'San Francisco', 'Portland', 'Seattle']
 
 export default function DestinationViewer() {
-  const offset = useRef(new Animated.Value(0)).current;
-
-  const panResponder = useRef(PanResponder.create({
-    onMoveShouldSetPanResponder: () => true,
-    onPanResponderGrant: () => {
-      offset.setValue(0);
-     },
-    onPanResponderMove: Animated.event([null, { dy: offset }], {useNativeDriver: false}),
-    onPanResponderRelease: () => { offset.stopAnimation((lastOffset) => { offset.setOffset(lastOffset); }); },
-  })).current;
-
 
   return (
     <View style={styles.wrapper}>
@@ -23,11 +13,9 @@ export default function DestinationViewer() {
         <Text>Hello</Text>
       </View>
       <View style={styles.body}>
-        <Animated.View style={{...styles.item, transform: [{ translateY: offset }]}} {...panResponder.panHandlers}>
-          <View />
-        </Animated.View>
-        <View style={styles.item} />
-        <View style={styles.item} />
+        <MainDrag />
+        <MainDrag />
+        <MainDrag />
       </View>
       {/* <ScrollView style={styles.body}>
         {destinations.map((destination) => (<Text key={destination}>{destination}</Text>))}
@@ -43,7 +31,6 @@ export default function DestinationViewer() {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    backgroundColor: 'red',
   },
   header: {
     flex: 1,
@@ -57,11 +44,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  item : {
-    backgroundColor: 'white',
-    height: '20%',
-    width: '30%',
-    borderColor: 'black',
-    borderWidth: 1,
-  }
 });
