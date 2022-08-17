c0c7ccd9c82942b98e0ffcc20fc2b574eb7df907
noattongi <noattongi@gmail.com>
2022-08-17 12:18:20 -0700

    implemented basic front end of archive

--- components/Archive/Archive.tsx
+++ components/Archive/Archive.tsx
@@ -1,10 +1,37 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import CompletedList from './CompletedList';
+import TrashList from './TrashList';
 
 export default function Archive() {
+
+  const [whichTab, setWhichTab] = useState('Completed')
+
   return (
     <View style={styles.container}>
-      <Text>Stuff</Text>
+      <View style={styles.tabs}>
+        <Pressable
+          onPress={() => setWhichTab('Completed')}
+        >
+          <Text>Completed</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => setWhichTab('Trash')}
+        >
+          <Text>Trash</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.items}>
+        {whichTab === 'Completed' &&
+          <CompletedList/>
+        }
+        {whichTab === 'Trash' &&
+          <TrashList/>
+        }
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +42,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  tabs: {
+    display: "flex",
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  items: {
+    flex: 7,
+  }
 });
