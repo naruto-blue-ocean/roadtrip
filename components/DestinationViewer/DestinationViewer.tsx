79f3da458fe8c2ce328601759ff7f845f30f1b22
Jason Chiou <jaschiou@gmail.com>
2022-08-18 13:05:30 -0700

    working POI connection

--- components/DestinationViewer/DestinationViewer.tsx
+++ components/DestinationViewer/DestinationViewer.tsx
@@ -15,74 +15,6 @@ const { width: SCREEN_WIDTH } = Dimensions.get("window");
 export default function DestinationViewer({route, navigation}) {
   // const navigation = useNavigation();
   const {tripName, tripId} = route.params;
-  const sampleTrip = {
-    id: 100,
-    destinations: [
-      {
-        id: 200,
-        cityName: 'San Francisco',
-        POIs: [
-          {
-            id: 1,
-            name: 'Golden Gate Bridge',
-            details: 'An iconic red bridge'
-          },
-          {
-            id: 2,
-            name: 'Dolores Park',
-            details: 'A city park'
-          },
-          {
-            id: 3,
-            name: 'Dumpling Home',
-            details: 'A Chinese restaurant specializing in dumplings'
-          }
-        ]
-      },
-      {
-        id: 300,
-        cityName: 'San Diego',
-        POIs: [
-          {
-            id: 4,
-            name: 'Legoland',
-            details: 'An amusement park featuring Legos'
-          },
-          {
-            id: 5,
-            name: 'Tacos El Gordo',
-            details: 'A famous Mexican restaurant'
-          },
-          {
-            id: 6,
-            name: 'UC San Diego',
-            details: 'A public university'
-          }
-        ]
-      },
-      {
-        id: 400,
-        cityName: 'Los Angeles',
-        POIs: [
-          {
-            id: 7,
-            name: 'Griffith Observatory',
-            details: 'An observatory with telecopes and exhibits'
-          },
-          {
-            id: 8,
-            name: 'Disneyland',
-            details: 'The happiest place on Earth'
-          },
-          {
-            id: 9,
-            name: 'Hollywood',
-            details: 'A hub for entertainment and media'
-          }
-        ]
-      }
-    ]
-  }
 
   const getTrip = (trip_id) => {
     const path = `${config.LOCALTUNNEL}/trips/destinations/${trip_id}`
@@ -229,7 +161,7 @@ export default function DestinationViewer({route, navigation}) {
       <View style = {styles.addAndShareContainer}>
         <Pressable style={styles.addCity}
           onPress = {() =>
-            navigation.navigate('AddCity', {city: 'San Francisco'})
+            navigation.navigate('AddCity', {trip_id: tripId})
           }
           >
           <Text>Add Destinations &nbsp;</Text>
@@ -337,3 +269,74 @@ const styles = StyleSheet.create({
     width: '50%'
   }
 });
+
+
+/*
+const sampleTrip = {
+    id: 100,
+    destinations: [
+      {
+        id: 200,
+        cityName: 'San Francisco',
+        POIs: [
+          {
+            id: 1,
+            name: 'Golden Gate Bridge',
+            details: 'An iconic red bridge'
+          },
+          {
+            id: 2,
+            name: 'Dolores Park',
+            details: 'A city park'
+          },
+          {
+            id: 3,
+            name: 'Dumpling Home',
+            details: 'A Chinese restaurant specializing in dumplings'
+          }
+        ]
+      },
+      {
+        id: 300,
+        cityName: 'San Diego',
+        POIs: [
+          {
+            id: 4,
+            name: 'Legoland',
+            details: 'An amusement park featuring Legos'
+          },
+          {
+            id: 5,
+            name: 'Tacos El Gordo',
+            details: 'A famous Mexican restaurant'
+          },
+          {
+            id: 6,
+            name: 'UC San Diego',
+            details: 'A public university'
+          }
+        ]
+      },
+      {
+        id: 400,
+        cityName: 'Los Angeles',
+        POIs: [
+          {
+            id: 7,
+            name: 'Griffith Observatory',
+            details: 'An observatory with telecopes and exhibits'
+          },
+          {
+            id: 8,
+            name: 'Disneyland',
+            details: 'The happiest place on Earth'
+          },
+          {
+            id: 9,
+            name: 'Hollywood',
+            details: 'A hub for entertainment and media'
+          }
+        ]
+      }
+    ]
+  } */
