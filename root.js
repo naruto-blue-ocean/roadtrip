4412ef3bd5b260ff5c38f37ae62a3e667f5f8254
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-18 09:26:47 -0700

    created splash screen

--- root.js
+++ root.js
@@ -1,26 +1,28 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FakeHomeScreen from './components/FakeHomeScreen/FakeHomeScreen';
-import PoiViewer from './components/POIViewer/PoiViewer';
-import DestinationViewer from './components/DestinationViewer/DestinationViewer';
-import AddCity from './components/AddCity/AddCityIndex';
-import HomeScreen from './components/HomeScreen/HomeScreen';
-import AddPOI from './components/AddPOI/AddPOI';
-import Initialization from './components/Initialization/Initialization';
-import Login from './components/Login/Login';
-import Archive from './components/Archive/Archive';
-import AuthProvider, { AuthContext } from './AuthProvider';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FakeHomeScreen from "./components/FakeHomeScreen/FakeHomeScreen";
+import PoiViewer from "./components/POIViewer/PoiViewer";
+import DestinationViewer from "./components/DestinationViewer/DestinationViewer";
+import AddCity from "./components/AddCity/AddCityIndex";
+import HomeScreen from "./components/HomeScreen/HomeScreen";
+import AddPOI from "./components/AddPOI/AddPOI";
+import Initialization from "./components/Initialization/Initialization";
+import Login from "./components/Login/Login";
+import Archive from "./components/Archive/Archive";
+import AuthProvider, { AuthContext } from "./AuthProvider";
 // import AuthProvider from './AuthProvider';
-import { QueryClient, QueryClientProvider, useQuery, useInfiniteQuery } from 'react-query';
-
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+  useInfiniteQuery,
+} from "react-query";
 
 const Stack = createNativeStackNavigator();
 const queryClient = new QueryClient();
 
-
 export default function Root() {
-
   const { isLoggedIn, setIsLoggedIn } = React.useContext(AuthContext);
   const [checking, setIsChecking] = React.useState(true);
 
@@ -89,47 +91,47 @@ export default function Root() {
   // )
   return (
     <QueryClientProvider client={queryClient}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="FakeHomeScreen">
-            <>
-              <Stack.Screen
-                name="FakeHomeScreen"
-                component={FakeHomeScreen}
-                options={{ title: 'Fake Home' }}
-              />
-              <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{ title: 'Home' }}
-              />
-              <Stack.Screen
-                name="Archive"
-                component={Archive}
-                options={{ title: 'View Archived Trips' }}
-              />
-              <Stack.Screen
-                name="DestinationViewer"
-                component={DestinationViewer}
-                options={{ title: 'Destinations' }}
-              />
-              {/* <Stack.Screen
-                name="POIViewer"
-                component={PoiViewer}
-                options={{ title: 'Points Of Interest' }}
-              /> */}
-              <Stack.Screen
-                name="AddCity"
-                component={AddCity}
-                options={{ title: 'Add a Destination' }}
-              />
-              <Stack.Screen
-                name="AddPOI"
-                component={AddPOI}
-                options={{ title: 'Add a Point of Interest' }}
-              />
-            </>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="FakeHomeScreen">
+          <>
+            <Stack.Screen
+              name="FakeHomeScreen"
+              component={FakeHomeScreen}
+              options={{ title: "Fake Home" }}
+            />
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ title: "Home" }}
+            />
+            <Stack.Screen
+              name="Archive"
+              component={Archive}
+              options={{ title: "View Archived Trips" }}
+            />
+            <Stack.Screen
+              name="DestinationViewer"
+              component={DestinationViewer}
+              options={{ title: "Destinations" }}
+            />
+            <Stack.Screen
+              name="POIViewer"
+              component={PoiViewer}
+              options={{ title: "Points Of Interest" }}
+            />
+            <Stack.Screen
+              name="AddCity"
+              component={AddCity}
+              options={{ title: "Add a Destination" }}
+            />
+            <Stack.Screen
+              name="AddPOI"
+              component={AddPOI}
+              options={{ title: "Add a Point of Interest" }}
+            />
+          </>
+        </Stack.Navigator>
+      </NavigationContainer>
     </QueryClientProvider>
-  )
+  );
 }
