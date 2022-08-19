71d8056109d02be92160e86565aae791a9b28dce
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-19 00:45:35 -0700

    able to connect the navigation

--- drawerScreens/Login.tsx
+++ drawerScreens/Login.tsx
@@ -8,6 +8,9 @@ import { useNavigation } from "@react-navigation/native"; //Need to import this
 import DrawerNavigatorRoutes from "./DrawerNavigatorRoutes";
 import HomeScreen from "../components/HomeScreen/HomeScreen";
 import DestinationViewer from "../components/DestinationViewer/DestinationViewer";
+import AddCity from "../components/AddCity/AddCityIndex";
+import PoiViewer from "../components/POIViewer/PoiViewer";
+import AddPOI from "../components/AddPOI/AddPOI";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +25,9 @@ export default function Login() {
     <Stack.Navigator initialRouteName="Home Screen">
       <Stack.Screen name= "Home Screen" children={() => <HomeScreen /> }/>
       <Stack.Screen name= "DestinationViewer" children={() => <DestinationViewer /> }/>
+      <Stack.Screen name= "AddCity" children={() => <AddCity /> }/>
+      <Stack.Screen name= "AddPOI" children={() => <AddPOI /> }/>
+      <Stack.Screen name= "PoiViewer" children={() => <PoiViewer /> }/>
 
     </Stack.Navigator>
     // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
