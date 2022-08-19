102f37d940832e34bead64f8f7944a5f25717fb6
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-18 23:22:32 -0700

    saved changed

--- root.js
+++ root.js
@@ -144,13 +144,12 @@ import AuthProvider, { AuthContext } from "./AuthProvider";
 //NEW THINGS TO IMPORT
 import FakeHomeScreen from "./components/FakeHomeScreen/FakeHomeScreen";
 import Login from "./components/Login/Login";
-<<<<<<< HEAD
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-=======
 import Archive from "./components/Archive/Archive";
 import AuthProvider, { AuthContext } from "./AuthProvider";
->>>>>>> 37364b4169a94b56799dffb324f6045167fc22b2
+
 import {
   QueryClient,
   QueryClientProvider,
@@ -190,8 +189,6 @@ export default function Root() {
 
   const [checking, setIsChecking] = React.useState(true);
 
-<<<<<<< HEAD
-=======
   // NOTICE: There are two versions of the return block, the first commented out block includes the login page and the second doesn't. If you want to switch, just comment/uncomment out one of the return blocks.
 
   // return (
@@ -251,13 +248,9 @@ export default function Root() {
   //   </QueryClientProvider>
   // )
 
-
->>>>>>> 37364b4169a94b56799dffb324f6045167fc22b2
   return (
-    // <AuthProvider>
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-<<<<<<< HEAD
         <Stack.Navigator initialRouteName="Login">
           {username !== null ? (
             <Stack.Screen
@@ -275,51 +268,48 @@ export default function Root() {
         </Stack.Navigator>
       </NavigationContainer>
     </QueryClientProvider>
-    // </AuthProvider>
   );
-=======
-        <Stack.Navigator initialRouteName="FakeHomeScreen">
-          <>
-            <Stack.Screen
-              name="FakeHomeScreen"
-              component={FakeHomeScreen}
-              options={{ title: 'Fake Home' }}
-            />
-            <Stack.Screen
-              name="HomeScreen"
-              component={HomeScreen}
-              options={{ title: 'Home' }}
-            />
-            <Stack.Screen
-              name="Archive"
-              component={Archive}
-              options={{ title: 'View Archived Trips' }}
-            />
-            <Stack.Screen
-              name="DestinationViewer"
-              component={DestinationViewer}
-              options={{ title: 'Destinations' }}
-            />
-            <Stack.Screen
-              name="POIViewer"
-              component={PoiViewer}
-              options={{ title: 'Points Of Interest' }}
-            />
-            <Stack.Screen
-              name="AddCity"
-              component={AddCity}
-              options={{ title: 'Add a Destination' }}
-            />
-            <Stack.Screen
-              name="AddPOI"
-              component={AddPOI}
-              options={{ title: 'Add a Point of Interest' }}
-            />
-          </>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </QueryClientProvider>
-  )
 
->>>>>>> 37364b4169a94b56799dffb324f6045167fc22b2
+  //       <Stack.Navigator initialRouteName="FakeHomeScreen">
+  //         <>
+  //           <Stack.Screen
+  //             name="FakeHomeScreen"
+  //             component={FakeHomeScreen}
+  //             options={{ title: 'Fake Home' }}
+  //           />
+  //           <Stack.Screen
+  //             name="HomeScreen"
+  //             component={HomeScreen}
+  //             options={{ title: 'Home' }}
+  //           />
+  //           <Stack.Screen
+  //             name="Archive"
+  //             component={Archive}
+  //             options={{ title: 'View Archived Trips' }}
+  //           />
+  //           <Stack.Screen
+  //             name="DestinationViewer"
+  //             component={DestinationViewer}
+  //             options={{ title: 'Destinations' }}
+  //           />
+  //           <Stack.Screen
+  //             name="POIViewer"
+  //             component={PoiViewer}
+  //             options={{ title: 'Points Of Interest' }}
+  //           />
+  //           <Stack.Screen
+  //             name="AddCity"
+  //             component={AddCity}
+  //             options={{ title: 'Add a Destination' }}
+  //           />
+  //           <Stack.Screen
+  //             name="AddPOI"
+  //             component={AddPOI}
+  //             options={{ title: 'Add a Point of Interest' }}
+  //           />
+  //         </>
+  //       </Stack.Navigator>
+  //     </NavigationContainer>
+  //   </QueryClientProvider>
+  // )
 }
