89a23f24602f24ad499d87b13edfbabec645d75d
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-18 23:56:32 -0700

    got the navigation portion to work!

--- drawerScreens/DrawerNavigatorRoutes.tsx
+++ drawerScreens/DrawerNavigatorRoutes.tsx
@@ -6,8 +6,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 //import the bottom tabs
 import AllTrips from "./bottomNavigation/AllTrips";
+import HomeScreen from "../components/HomeScreen/HomeScreen";
 import ActiveTrip from "./bottomNavigation/ActiveTrip";
-import Archive from "./bottomNavigation/Archive";
+// import Archive from "./bottomNavigation/Archive";
+import Archive from "../components/Archive/Archive";
 import { ScreenStackHeaderBackButtonImage } from "react-native-screens";
 import { AuthContext } from "../AuthProvider";
 
@@ -21,7 +23,7 @@ export default function DrawerNavigatorRoutes() {
 
   return (
     <Bottom.Navigator  initialRouteName="All Trips">
-      <Bottom.Screen name="All Trips"   children={() => (<AllTrips /> )} />
+      <Bottom.Screen name="All Trips"   children={() => (<HomeScreen  /> )} />
 
       <Bottom.Screen name="Active Trip"children={() => (<ActiveTrip /> )} />
 
