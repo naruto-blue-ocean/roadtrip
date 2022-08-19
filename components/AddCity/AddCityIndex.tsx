7ed712f5dca0de3d7e6d95c085df2a9ffad0658a
JASPER <jasjasper101@gmail.com>
2022-08-19 13:56:16 -0700

    change color

--- components/AddCity/AddCityIndex.tsx
+++ components/AddCity/AddCityIndex.tsx
@@ -58,7 +58,7 @@ var styles = StyleSheet.create({
     padding: 10,
     flex: 1,
     alignContent: 'center',
-    backgroundColor: '#F0E8E4',
+    backgroundColor: '#FFFFFF',
   },
 
   cityContainer : {
@@ -81,7 +81,7 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     width: 200,
     borderRadius: 20,
-    backgroundColor: '#D9814F'
+    backgroundColor: '#B0C4B1'
   },
 
   text: {
