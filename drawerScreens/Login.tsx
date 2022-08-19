8f4d1299e309bf9061660523b9b4c829abb47c71
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-19 00:27:58 -0700

    created another file for stack screen

--- drawerScreens/Login.tsx
+++ drawerScreens/Login.tsx
@@ -3,8 +3,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native"
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native"; //Need to import this
 import DrawerNavigatorRoutes from "./DrawerNavigatorRoutes";
+import HomeScreen from "../components/HomeScreen/HomeScreen";
+import DestinationViewer from "../components/DestinationViewer/DestinationViewer";
+
+const Stack = createNativeStackNavigator();
 
 export default function Login() {
   const navigation = useNavigation();
@@ -14,11 +19,16 @@ export default function Login() {
  /*change navigate to replace when compelete */
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Pressable
-      onPress={() => {navigation.navigate("DrawerNavigatorRoutes")}}>
-      <Text>This is the Login Screen</Text>
-      </Pressable>
-    </View>
+    <Stack.Navigator initialRouteName="Home Screen">
+      <Stack.Screen name= "Home Screen" children={() => <HomeScreen /> }/>
+      <Stack.Screen name= "DestinationViewer" children={() => <DestinationViewer /> }/>
+
+    </Stack.Navigator>
+    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //   <Pressable
+    //   onPress={() => {navigation.navigate("DrawerNavigatorRoutes")}}>
+    //   <Text>This is the Login Screen</Text>
+    //   </Pressable>
+    // </View>
   )
 }
