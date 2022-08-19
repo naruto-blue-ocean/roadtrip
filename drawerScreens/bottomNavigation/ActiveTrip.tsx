5610e42420d1c6fc8dfb89171077870a69db0cad
Jason Chiou <jaschiou@gmail.com>
2022-08-19 13:30:47 -0700

    yo this thing is kinda working now!!!!

--- drawerScreens/bottomNavigation/ActiveTrip.tsx
+++ drawerScreens/bottomNavigation/ActiveTrip.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native"
 import { createStackNavigator } from "@react-navigation/stack";
+import DestinationViewer from "../../components/DestinationViewer/DestinationViewer";
+import axios from 'axios';
+import config from '../../config.js'
+import { AuthContext } from '../../AuthProvider.js'
 
 const Stack = createStackNavigator();
 
 export default function ActiveTrip() {
 
+  const { username } = useContext(AuthContext);
+  let tripId = 1;
+
+  const path = `${config.LOCALTUNNEL}/trips/${username}/active`
+  axios.get(path)
+  .then((response) => {
+    let tripId = response.data.id
+  })
+  .catch((err) => {
+    console.error('errored in gettingActiveTrip', err)
+  })
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>All Active Trip go here!</Text>
+      <DestinationViewer tripId = {tripId} />
     </View>
   )
 }
